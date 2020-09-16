b4b8ca23367498f15066e8f9e011209db8957ed8
MrPanda05 <69354591+MrPanda05@users.noreply.github.com>
2020-09-16 10:14:16 -0300

    Enter

--- commands/ping.js
+++ commands/ping.js
@@ -3,6 +3,13 @@ module.exports = {
 	description: 'Ping!',
 	cooldown: 5,
 	execute(message) {
-		message.channel.send(`Poggers`);
+		
+		message.channel.send({embed: {
+			color: 3447003,
+			description: "Poggers",
+			author: {
+				name: message.author.username
+			}
+		  }});
 	},
 };
